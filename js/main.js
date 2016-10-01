d72b89588f6f80915325bf7b06259b9d36f734c7
Wolf <wolf.wikgren@gmail.com>
2016-10-02 02:39:07 +0300

    Basic window functionality

--- js/main.js
+++ js/main.js
@@ -1,18 +1,54 @@
-_386 = { onePass: true, speedFactor: 1.25 };
-  $( function() {
-    $( ".draggable" ).draggable();
-  } );
+_386 = {
+  onePass: true,
+  speedFactor: 1.25
+};
+$(function() {
+  $(".draggable").draggable();
+});
 
-   $( function() {
-    $( ".resizable" ).resizable({
-      grid: 50
-    });
-  } );
+$(function() {
+  $(".resizable").resizable({
+    grid: 50,
+    helper: "ui-resizable-helper",
+    stop: function(e, ui) {
+      console.log(ui);
+      var scroll = $('.jsscroll').jScrollPane(scrollSettings);
+      var contentPane = scrollApi.getContentPane();
+      $(".content-modal").width(ui.size.width);
+      $(".jspContainer").width(ui.size.width);
+      $(".jspPane").width(ui.size.width);
+      $(".jspTrack").width(ui.size.width);
+      $(".jspContainer").height(ui.size.height - 5);
+    }
+  });
+});
 
-   $('.dither').ditherJS(options);
+$(".content-modal-close").click(function() {
+  $(this).closest('.content-modal').hide();
+});
+
+$(".application-icon").dblclick(function() {
+  var modalToOpen = $(this).data('openModal');
+  $('#' + modalToOpen).show();
+});
+
+$("#scrollbars").click(function() {
+  scrollApi.reinitialise();
+});
+
+$('.dither').ditherJS(options);
 
 var options = {
-    "step": 1,
-    "className": "dither",
-    "algorithm": "atkinson"
+  "step": 1,
+  "className": "dither",
+  "algorithm": "atkinson"
 };
+var scrollSettings = {
+  alwaysShowScroll: true,
+  showArrows: true,
+  verticalGutter: 0
+
+};
+var scroll = $('.jsscroll').jScrollPane(scrollSettings);
+$('.jsscroll .jspContainer').append('<div class="content-modal-footer"></div>');
+var scrollApi = scroll.data('jsp');
