e3f954f3ce9ed2c7366561a9205716c49b5cb1bb
Wolf <wolf.wikgren@gmail.com>
2016-10-02 03:59:38 +0300

    Font changes, minor graphical fixes

--- js/main.js
+++ js/main.js
@@ -7,18 +7,42 @@ $(function() {
 });
 
 $(function() {
-  $(".resizable").resizable({
+  $("#juttu1").resizable({
     grid: 50,
     helper: "ui-resizable-helper",
     stop: function(e, ui) {
       console.log(ui);
       var scroll = $('.jsscroll').jScrollPane(scrollSettings);
       var contentPane = scrollApi.getContentPane();
-      $(".content-modal").width(ui.size.width);
-      $(".jspContainer").width(ui.size.width);
-      $(".jspPane").width(ui.size.width);
-      $(".jspTrack").width(ui.size.width);
-      $(".jspContainer").height(ui.size.height - 5);
+      $(this).width(ui.size.width);
+      $(".content-modal", this).width(ui.size.width);
+      $(".jspContainer", this).width(ui.size.width);
+      $(".jspPane", this).width(ui.size.width);
+      $(".jspTrack", this).width(ui.size.width);
+      $(".jspContainer", this).height(ui.size.height - 5);
+    }
+  });
+});
+
+$(function() {
+  $("#juttu2").resizable({
+    grid: 50,
+    helper: "ui-resizable-helper",
+    stop: function(e, ui) {
+      var scroll = $('.jsscroll').jScrollPane(scrollSettings);
+      var contentPane = scrollApi.getContentPane();
+      $(this).width(ui.size.width);
+      $(".content-modal", this).width(ui.size.width);
+      $(".jspContainer", this).width(ui.size.width);
+      $(".jspPane", this).width(ui.size.width);
+      $(".jspTrack", this).width(ui.size.width);
+      $(".jspContainer", this).height(ui.size.height - 5);
+      /*
+            $(".content-modal").width(ui.size.width);
+            $(".jspContainer").width(ui.size.width);
+            $(".jspPane").width(ui.size.width);
+            $(".jspTrack").width(ui.size.width);
+            $(".jspContainer").height(ui.size.height - 5);*/
     }
   });
 });
@@ -30,6 +54,8 @@ $(".content-modal-close").click(function() {
 $(".application-icon").dblclick(function() {
   var modalToOpen = $(this).data('openModal');
   $('#' + modalToOpen).show();
+  $('.active', 'body').removeClass('active');
+  $('#' + modalToOpen).addClass("active");
 });
 
 $("#scrollbars").click(function() {
@@ -52,3 +78,8 @@ var scrollSettings = {
 var scroll = $('.jsscroll').jScrollPane(scrollSettings);
 $('.jsscroll .jspContainer').append('<div class="content-modal-footer"></div>');
 var scrollApi = scroll.data('jsp');
+
+$(".content-modal").click(function() {
+  $('.active', 'body').removeClass('active');
+  $(this).addClass("active");
+});
